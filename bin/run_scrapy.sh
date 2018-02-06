ba87b93a07eb0a9e6103768d254b1292d84eaab3
PriyaRanjan Prakash <pprakash@blazent.com>
2018-02-06 12:27:42 +0530

    merged data

--- bin/run_scrapy.sh
+++ bin/run_scrapy.sh
@@ -3,14 +3,16 @@
 usage="$0 <spider_name> <output filename> <output filetype>\n"
 example="$0 cisco cisco.csv csv\n"
 
-[[ $# -ne 3 ]] && echo -e "\n$usage\n$example\n" && exit 1
+#[[ $# -ne 3 ]] && echo -e "\n$usage\n$example\n" && exit 1
 spider_name="$1"
 file="$2"
 file_type="$3"
+properties_file="$4"
 
 # Source the property file for pre-defined variables
-[[ ! -s scrapy.properties ]] && echo -e "\nError: scrapy.properties file does not exist. Exiting...\n" && exit 1
-. scrapy.properties
+
+[[ ! -s "$properties_file" ]] && echo -e "\nError: scrapy.properties file does not exist. Exiting...\n" && exit 1
+. "$properties_file"
 
 export PYTHONPATH="${PYTHONPATH}:${BLZ_PROJ_DIR}/"
 spider_location="${BLZ_BOT_DIR}/${spider_name}/src"
