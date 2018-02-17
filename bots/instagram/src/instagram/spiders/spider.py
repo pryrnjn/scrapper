1361347ebc3df6ebcb5d761d9f9edc31e200c478
PriyaRanjan Prakash <pprakash@blazent.com>
2018-02-17 09:52:17 +0530

    update

--- bots/instagram/src/instagram/spiders/spider.py
+++ bots/instagram/src/instagram/spiders/spider.py
@@ -22,7 +22,7 @@ class InstagramSpider(Spider):
 
     def __init__(self, *args, **kwargs):
         super(InstagramSpider, self).__init__(*args, **kwargs)
-        self.driver = get_chrome_browser(False, 5)
+        self.driver = get_chrome_browser(True, 5)
         self.max_count = 10
         self.requests_processed = dict()
         self.loaded = dict()
